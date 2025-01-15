039e63b1cd34329889bf4677591d7f06ff31a84f
Magdalena Hönickl <s2310456006@fhooe.at>
2025-01-15 18:09:39 +0100

    Commit final

--- packages/biobauernhof/ext_localconf.php
+++ packages/biobauernhof/ext_localconf.php
@@ -11,3 +11,6 @@ $GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['biobauernhof'] = 'EXT:biobauernho
  * PageTS
  */
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:biobauernhof/Configuration/TsConfig/Page/All.tsconfig">');
+
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['locallangXMLOverride']['EXT:news/Resources/Private/Language/locallang.xlf'][] = 'EXT:biobauernhof/Resources/Private/Language/locallang.xlf';
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['locallangXMLOverride']['de']['EXT:news/Resources/Private/Language/locallang.xlf'][] = 'EXT:biobauernhof/Resources/Private/Language/de.locallang.xlf';
